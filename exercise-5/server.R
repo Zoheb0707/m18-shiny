867f1e30a60fd2e2db3a5d408ae95b04aa7ba45c
Zoheb0707 <zhs142013@yahoo.in>
2017-05-17 14:06:24 -0700

    final commit

--- exercise-5/server.R
+++ exercise-5/server.R
@@ -8,9 +8,7 @@ library(HSAUR)
 library(dplyr)
 library(ggplot2)
 #Load the dataset 'womensrole' from the HSUAR package (same way you would load a built in dataset)
-
-
-
+data.set <- womensrole
 #Here you will define the data that is shown based on your inputs defined in the UI
 #and turn it into an output plot
 #Hint: reference the input values using 'input$inputID'
@@ -26,17 +24,29 @@ shinyServer(function(input, output) {
     
     
     #Filter the dataset based on whether the input is Men, Women, or Both
-
-  
+     if(input$sex == 'Women') {
+         plot.data <- data.set %>% filter(sex == 'Female')
+     }
+     else if(input$sex == 'Men') {
+         plot.data <- data.set %>% filter(sex == 'Male')
+     }
+     else {
+         plot.data <- data.set
+     }
     
     #Define a y axis value based on the input Agree or Disagree
-
+     if(input$thoughts == 'Agree') {
+         thoughts = plot.data$agree
+     }
+     else {
+      thoughts = plot.data$disagree
+     }
     
     #Using the values you just defined, construct a scatterplot using ggplot2
     #x will be defined by education, y by agree/disagree
     #Bonus: Factor the color field by sex 
     #(this will allow you to visually see the difference between Men and Women when Both are selected)
-
+    ggplot(plot.data, x = education, y = thoughts, color = factor(sex)) + geom_point()
     
   })
   
